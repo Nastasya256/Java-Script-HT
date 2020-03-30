21baa6912dad27559aa6aea5e47700dc6c731838
Nastasya Zhivaeva <nastasyazzhh@gmail.com>
2020-03-30 17:39:46 +0300

    Задание 4 на массив

---
+++ case.js
@@ -0,0 +1,3 @@
+let str = prompt("Ваше имя?");
+let newStr = str[0].toUpperCase() + str.slice(1);
+console.log(newStr);
---
+++ massiv.js
@@ -0,0 +1,4 @@
+let arr= ['dog', 'cat', 'rabbit', 'mose'];
+      arr.splice(1, 1)
+
+console.log(arr);
